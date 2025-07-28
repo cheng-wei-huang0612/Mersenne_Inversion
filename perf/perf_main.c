21d790b524755b75199b520a923b8c6c54c73b99
Cesare Huang <Cesarehuang@icloud.com>
2025-07-28 14:48:23 +0800

    perf analysis

--- perf/perf_main.c
+++ perf/perf_main.c
@@ -1,16 +1,13 @@
-
-
-
 #include <stdint.h>
-extern void inverse(uint64_t inv[static 4], const uint64_t x[static 4]);
+extern void inverse(uint64_t[static 4], const uint64_t[static 4]);
 
-
-int main(int argc, char const *argv[])
+int main(void)
 {
-    uint64_t x[4] = {0};
-    uint64_t inv[4] = {0};
+    uint64_t x[4] = {1,0,0,0};
+    uint64_t inv[4];
 
-    inverse(inv, x);
+    for (unsigned i = 0; i < 100000; i++)   // ← 10^5 次大約 10~20 ms
+        inverse(inv, x);
 
     return 0;
 }
