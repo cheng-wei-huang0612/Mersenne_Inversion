4ce3b48ac82e5e830de4eaf4121467d52198575c
Cesare Huang <Cesarehuang@icloud.com>
2025-08-07 13:37:56 +0800

    debug

--- src/python/asm_4_optimizer_a72.py
+++ src/python/asm_4_optimizer_a72.py
@@ -21,7 +21,8 @@ slothy = Slothy(arch, target)
 slothy.load_source_from_file("../asm/inverse_4.s")
 slothy.config.variable_size = True
 slothy.config.constraints.stalls_first_attempt = 32
-slothy.config.reserved_regs = ["x0", "x19", "sp"]
+slothy.config.reserved_regs = ["x0", "x19", "sp",
+                               "v1","v2","v15"]
 slothy.config.reserved_regs_are_locked = True
 
 slothy.config.inputs_are_outputs = True
@@ -35,20 +36,31 @@ slothy.config.inputs_are_outputs = True
 
 slothy.config.inputs_are_outputs = True
 slothy.config.outputs = ["x1","x2","x3",
+                         "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12",
                          "v13", "v14"]
 
 
 
 slothy.optimize("Lbig_loop","Lend")
 
+slothy.config.reserved_regs = ["x0", "x19", "sp",
+                               "v1","v2","v8","v9","v10","v11","v12",
+                               "v15"]
+slothy.config.reserved_regs_are_locked = True
 slothy.config.outputs = ["x1","x2","x3","x4","x5","x21","x22",
-                         "v13", "v14"]
+                         "v3","v4","v13", "v14"]
 
 slothy.optimize("L_optloop_start_1", "L_optloop_end_1")
 
 
+
+
+slothy.config.reserved_regs = ["x0", "x19", "sp",
+                               "v1","v2","v3","v4","v5","v6","v7",
+                               "v15"]
+slothy.config.reserved_regs_are_locked = True
 slothy.config.outputs = ["x1","x2","x3","x4","x5","x21","x22",
-                         "v13", "v14"]
+                         "v3","v4","v13", "v14"]
 
 slothy.optimize("L_optloop_start_2", "L_optloop_end_2")
 
